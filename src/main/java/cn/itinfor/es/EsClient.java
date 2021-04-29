98f7753893be8725d2f346a8d80e957e07e460ee
paopao7 <980569038@qq.com>
2021-04-30 01:02:42 +0800

    初始化链接完成

--- src/main/java/cn/itinfor/es/EsClient.java
+++ src/main/java/cn/itinfor/es/EsClient.java
@@ -1,8 +1,15 @@
 package cn.itinfor.es;
 
-public class EsClient {
-    public static void main(String[] args) {
-        //创建es客户端
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 
+public class EsClient {
+    public static void main(String[] args) throws Exception {
+        //创建es客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+        esClient.close();
     }
 }
