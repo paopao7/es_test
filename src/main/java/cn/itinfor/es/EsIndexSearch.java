d2c51da56a5cb9454afab978ee76d2f40ff6fb4d
qianchao <980569038@qq.com>
2021-05-07 15:36:46 +0800

    追加查询索引

---
+++ src/main/java/cn/itinfor/es/EsIndexSearch.java
@@ -0,0 +1,34 @@
+package cn.itinfor.es;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+
+/**
+ * 查询索引
+ */
+public class EsIndexSearch {
+    public static void main(String[] args) throws Exception {
+        //创建es客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        //查询索引
+        GetIndexRequest request = new GetIndexRequest("user");
+        GetIndexResponse getIndexResponse = esClient.indices().get(request, RequestOptions.DEFAULT);
+
+        //响应状态
+        System.out.println(getIndexResponse.getAliases());
+        System.out.println(getIndexResponse.getMappings());
+        System.out.println(getIndexResponse.getSettings());
+
+        //关闭es客户端
+        esClient.close();
+    }
+}
