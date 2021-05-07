2dc22d70445e551cc5cef66ecfa67a7192c9f149
qianchao <980569038@qq.com>
2021-05-07 15:50:57 +0800

    添加插入数据

---
+++ src/main/java/cn/itinfor/es/EsDocInsert.java
@@ -0,0 +1,46 @@
+package cn.itinfor.es;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.common.xcontent.XContentType;
+
+/**
+ * 插入数据
+ */
+public class EsDocInsert {
+    public static void main(String[] args) throws Exception {
+        //创建es客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        //插入数据
+        IndexRequest request = new IndexRequest();
+        request.index("user").id("1001");
+
+        User user = new User();
+        user.setName("张三");
+        user.setAge(30);
+        user.setSex("男");
+
+        //向ES插入数据，必须转换为json格式
+        ObjectMapper mapper = new ObjectMapper();
+        String userJson = mapper.writeValueAsString(user);
+
+        request.source(userJson, XContentType.JSON);
+
+        IndexResponse response = esClient.index(request, RequestOptions.DEFAULT);
+
+        System.out.println(response.getResult());
+
+        //关闭es客户端
+        esClient.close();
+    }
+}
