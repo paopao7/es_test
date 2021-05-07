068fd04826c5839cdb80710d39c7ef279f739019
qianchao <980569038@qq.com>
2021-05-07 15:41:48 +0800

    追加删除索引

---
+++ src/main/java/cn/itinfor/es/EsIndexDelete.java
@@ -0,0 +1,32 @@
+package cn.itinfor.es;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+
+/**
+ * 删除索引
+ */
+public class EsIndexDelete {
+    public static void main(String[] args) throws Exception {
+        //创建es客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        //删除索引
+        DeleteIndexRequest request = new DeleteIndexRequest("user");
+        AcknowledgedResponse response = esClient.indices().delete(request, RequestOptions.DEFAULT);
+
+        //响应状态
+        System.out.println(response.isAcknowledged());
+
+        //关闭es客户端
+        esClient.close();
+    }
+}
